2d0266af03fe19ef422f4fa395c56510cc7a9563
Sasa Nikolic <nikolic.sasa09@gmail.com>
2020-12-28 23:57:21 +0100

    Refactor to use EmailMessage and add code comments

--- scraper.py
+++ scraper.py
@@ -1,39 +1,43 @@
 import requests
 from bs4 import BeautifulSoup
 import smtplib, ssl
+from email.message import EmailMessage
 
-port = 465  # For SSL
+ # Port for SSL.
+port = 465 
 smtp_server = "smtp.gmail.com"
-sender_email = "6rodney6@gmail.com"
-receiver_email = "6rodney6@gmail.com"
-message = """\
-Subject: Canyon bikes
-
-The bikes available are: \n"""
+# Construct email.
+email = EmailMessage()
+email['Subject'] = 'Canyon bikes'
+email['From'] = "6rodney6@gmail.com"
+email['To'] = "6rodney6@gmail.com"
 
 URL = 'https://www.canyon.com/en-si/outlet/road-bikes/?cgid=outlet-road&prefn1=pc_familie&prefn2=pc_rahmengroesse&prefv1=Ultimate&prefv2=XL'
 page = requests.get(URL)
-
 soup = BeautifulSoup(page.content, 'html.parser')
 
+# Find the bikes.
 bikes = soup.find_all('li', class_='productGrid__listItem')
 
 count=0
+msg = ''
 for bike in bikes:
     count += 1
-    # Each job_elem is a new BeautifulSoup object.
-    # You can use the same methods on it as you did before.
+    # Each bike is a new BeautifulSoup object.
     title_elem = bike.find('div', class_='productTile__productName')
     price_elem = bike.find('div', class_='productTile__priceSale')
-    message += title_elem.text.strip() + ': ' + price_elem.text.strip() + '\n'
+    msg += title_elem.text.strip() + ': <strong>' + price_elem.text.strip() + '</strong><br>'
 
-# Send email here
+email.set_content('The bikes available are ' + str(count) + ':<br> ' + msg, subtype='html')
+
+# Let the user enter the email password.
 password = input("Type your password and press enter: ")
 
-# Create a secure SSL context
+# Create a secure SSL context and send the email.
 context = ssl.create_default_context()
 with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-    server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message.encode())
+    server.login(email['From'], password)
+    server.send_message(email)
 
-print('Email was sent to ' + receiver_email)
+# Success message.
+print('Email was sent to ' + email['To'])
